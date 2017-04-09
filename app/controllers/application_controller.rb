e75975155b3f36c6b256faf08b971f0a8feff93c
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-09 11:30:01 +0900

    add after_sign_in_path_for to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    '/'
+  end
+
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
