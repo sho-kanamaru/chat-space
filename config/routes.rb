68accdd318f222975a397e999ac73c92637c121e
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-10 11:27:31 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :groups, only: [:new, :edit] do
     resources :messages, only: [:index]
   end
