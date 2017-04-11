f349ce92775036c9acf04223c107a4c057aa3e7f
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-11 21:36:46 +0900

    create create_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,14 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    Group.create(group_params)
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
