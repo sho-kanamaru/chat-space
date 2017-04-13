317103d3edb82aa8f636274430f7b8efce46aaa8
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-13 18:25:31 +0900

    create edit_action at group_contoroller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,10 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def update
     group = Group.find(params[:id])
     group.update(group_params)
