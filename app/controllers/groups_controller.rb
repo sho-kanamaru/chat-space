6ed2326c25b858cc4057d5016724b14de5eb0385
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-13 18:26:17 +0900

    add strong_parameter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,6 +30,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
