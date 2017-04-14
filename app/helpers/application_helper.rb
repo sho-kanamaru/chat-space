4adf3bb2e7197fc4e172b97e3745d69d884379a1
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-14 23:14:23 +0900

    edit application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
   def form_title
-    if current_page?(new_group_path)
-      return "新規チャットグループ作成"
-    else
-      return "チャットグループ編集"
-    end
+     current_page?(new_group_path) ? "新規チャットグループ作成" : "チャットグループ編集"
   end
 end
