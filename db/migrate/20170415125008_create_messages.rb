8ea2c871e7f1dfcb6a208078726cacd0654f40cb
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-15 22:11:39 +0900

    create messages_table

--- db/migrate/20170415125008_create_messages.rb
+++ db/migrate/20170415125008_create_messages.rb
@@ -1,8 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
-      t.timestamps
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.text :body
+      t.string :image
+      t.timestamps null: false
     end
   end
 end
