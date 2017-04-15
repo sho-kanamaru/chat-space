9da284b3a6b240d47561a0b115d620f28efdf353
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-15 22:27:32 +0900

    define latest_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :groups_users
   has_many :users, through: :groups_users
   has_many :messages
+
+  def latest_message
+    if self.messages.exists?
+      return self.messages.order("created_at DESC")[0].body
+    else
+      return "まだメッセージはありません"
+    end
+  end
 end
