200cdff3fb3e5de639a7aaefe697e6c6ed65584e
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-15 21:48:11 +0900

    create create_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,17 @@ class MessagesController < ApplicationController
     @groups = current_user.groups.order("id ASC")
     @group = Group.find(params[:group_id])
   end
+
+  def create
+    @groups = current_user.groups.order("id ASC")
+    @group = Group.find(params[:group_id])
+    @message = Message.create(message_params)
+    redirect_to group_messages_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
 end
