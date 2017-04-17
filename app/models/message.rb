3d8c19c91a95f9817154434d7e0d4ef92fdab46b
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-17 20:22:56 +0900

    add validation of messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  validates :body, presence: true
+  validates :user_id, presence: true
+  validates :group_id, presence: true
 end
