d0ad747d3d9b1feddb735ead501ed0e265e9c9d0
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-19 00:06:42 +0900

    add flash_message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.create(message_params)
-    redirect_to group_messages_path
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, flash: {notice: "メッセージ送信成功"}
+    else
+      flash.now[:alert] = "メッセージ送信失敗"
+      render "index"
+    end
   end
 
   private
