a45d50468de67fc0913f3e994ee02e9820ec9586
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-22 12:19:22 +0900

    delete validation of user_id & group_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,4 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :body, presence: true
-  validates :user_id, presence: true
-  validates :group_id, presence: true
 end
