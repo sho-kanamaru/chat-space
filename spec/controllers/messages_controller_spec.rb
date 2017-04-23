1afa68aba5176f3d0e75e6ec937d58b846af7a49
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-23 09:22:00 +0900

    add space at messages_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -8,22 +8,22 @@ describe MessagesController, type: :controller do
   end
   let(:group) { create(:group) }
   let(:message) { create(:message, group_id: group.id, user_id: user.id) }
-  let(:messages) {create_list(:message, 3, group_id: group.id, user_id: user.id) }
+  let(:messages) { create_list(:message, 3, group_id: group.id, user_id: user.id) }
 
   describe 'GET #index' do
 
     it "assigns the requested group to @group" do
-      get :index, params: {group_id: group }
+      get :index, params: { group_id: group }
       expect(assigns(:group)). to eq group
      end
 
     it "assigns the requested messages to @messages" do
-      get :index, params: {group_id: group }
+      get :index, params: { group_id: group }
       expect(assigns(:messages)).to match(messages)
     end
 
     it "renders the :index template" do
-      get :index, params: {group_id: group }
+      get :index, params: { group_id: group }
       expect(response).to render_template :index
     end
 
@@ -37,7 +37,7 @@ describe MessagesController, type: :controller do
     end
 
     it "renders the :create template" do
-      post :create, params: {message: {body: message.body}, group_id: group, user_id: user }
+      post :create, params: { message: {body: message.body}, group_id: group, user_id: user }
       expect(response).to redirect_to group_messages_path
     end
   end
