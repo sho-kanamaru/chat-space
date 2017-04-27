a97065adf023fbf30911e7613777320e5ff62443
KANAMARU SHO <sho02.soccer@gmail.com>
2017-04-27 16:09:06 +0900

    edit messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
     if @message.save
     flash[:notice] = "メッセージ送信成功"
       respond_to do |format|
@@ -22,7 +22,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body).merge(group_id: params[:group_id])
   end
 
   def set_groups
