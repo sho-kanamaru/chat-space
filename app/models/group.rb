614089d5195643f39f24472376f6a87dee9ca642
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2017-05-03 16:36:41 +0000

    delete delete_method

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,10 +7,4 @@ class Group < ApplicationRecord
   def latest_message
     self.messages.last.try(:body) || "まだメッセージはありません"
   end
-
-  def delete(current_user)
-    other_users = self.users.select{|user| user != current_user}
-    return other_users
-  end
-  
 end
