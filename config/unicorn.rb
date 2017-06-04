7fac1cc13d9b5249c587ff1eebc0aaba530ec9c9
Your Name <you@example.com>
2017-06-04 01:52:02 +0000

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-app_path = File.expand_path('../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 working_directory app_path
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
