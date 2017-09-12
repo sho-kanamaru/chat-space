c4df4a086e91beeb7f99d773b4f3b04641b70e00
金丸翔 <sho@sho-kanamaru.local>
2017-09-13 02:13:58 +0900

    delete buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,4 @@
 $(function() {
-  function buildHTML(message) {
-
-    var image = `<div class="right-box__message__box__detail--image">
-                  <img src=${ message.image } alt= "image">
-                </div>`;
-
-    if(message.body && message.image) {
-      var html = `<div class="right-box__message__box__detail" data-id='${ message.data }'>
-                    <div class="right-box__message__box__detail--name">
-                      ${ message.name }
-                    </div>
-                    <div class="right-box__message__box__detail--date">
-                      ${ message.date }
-                    </div>
-                    <div class="right-box__message__box__detail--content">
-                      ${ message.body }
-                    </div>
-                    ${ image }
-                  </div>`;
-    } else if (message.body) {
-      var html = `<div class="right-box__message__box__detail" data-id='${ message.data }'>
-                    <div class="right-box__message__box__detail--name">
-                      ${ message.name }
-                    </div>
-                    <div class="right-box__message__box__detail--date">
-                      ${ message.date }
-                    </div>
-                    <div class="right-box__message__box__detail--content">
-                      ${ message.body }
-                    </div>
-                  </div>`;
-    } else if (message.image) {
-      var html = `<div class="right-box__message__box__detail" data-id='${ message.data }'>
-                    <div class="right-box__message__box__detail--name">
-                      ${ message.name }
-                    </div>
-                    <div class="right-box__message__box__detail--date">
-                      ${ message.date }
-                    </div>
-                    <div class="right-box__message__box__detail--content">
-                    </div>
-                    ${ image }
-                  </div>`;
-    }
-      return html;
-  }
-
   function buildflash(message) {
     var flash = `<div class="notice">
                   ${ message.flash }
@@ -53,41 +6,8 @@ $(function() {
     return flash;
   }
 
-  if(document.URL.match("/messages")) {
-    setInterval(autoload, 10000);
-  }
-
   function scroll_to_bottom(target_id) {
     $('.right-box__message').animate({scrollTop: target_id.offset().top});
-  }
-
-  function autoload(){
-
-    var current_url = location.href;
-    var last_message_id = $(".right-box__message__box__detail:last-child").data("id");
-
-    $.ajax({
-      type: 'GET',
-      url: current_url,
-      data: {
-        last_message_id: last_message_id
-      },
-      dataType: 'json'
-    })
-
-    .done(function(data) {
-      $.each(data, function(i, message) {
-        var html = buildHTML(message);
-        $('.right-box__message__box').append(html);
-        if(data.length){
-          var last_message_id = $(".right-box__message__box__detail:last-child");
-          scroll_to_bottom(last_message_id);
-        }
-      });
-    })
-    .fail(function() {
-      alert('error');
-    });
   }
 
   $('#new_message').on('submit', function(e){
@@ -106,8 +26,7 @@ $(function() {
     })
 
     .done(function(data) {
-      var html = buildHTML(data);
-      $('.right-box__message__box').append(html);
+      App.chat.put_message(data);
 
       var last_message_id = $(".right-box__message__box__detail:last-child");
       scroll_to_bottom(last_message_id);
