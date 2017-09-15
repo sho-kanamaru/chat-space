23e44843e6923e1d5a1e2b7a2946f7706fdf58b5
金丸翔 <sho@sho-kanamaru.local>
2017-09-15 11:57:54 +0900

    add association

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ApplicationRecord
   has_many :groups_users
   has_many :users, through: :groups_users
   has_many :messages
+  has_many :message_recipients
 
   def latest_message
     if self.messages.last == nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :groups_users
   has_many :groups, through: :groups_users
   has_many :messages
+  has_many :message_recipients
   validates :name, presence: true
 
   scope :not_current_users, ->(group, user) { joins(:groups_users).merge(GroupsUser.where(["group_id = ? and user_id != ?", group, user])) }
